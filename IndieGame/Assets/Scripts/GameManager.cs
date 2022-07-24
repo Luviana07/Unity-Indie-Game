e6d78f61fcbe9332a67e1171222907776b41049e
Ufuk Kalkan <ufukkalkan07@gmail.com>
2022-07-24 03:39:56 +0200

    Unity Indie Game
    Indie Game With Unity

---
+++ IndieGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,174 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+
+    private void Awake()
+    {
+        if (GameManager.instance != null)
+        {
+            Destroy(gameObject);
+            Destroy(player.gameObject);
+            Destroy(floatingTextManager.gameObject);
+            Destroy(hud);
+            Destroy(menu);
+            return;
+        }
+
+        
+
+        instance = this;
+        SceneManager.sceneLoaded += LoadState;
+        SceneManager.sceneLoaded += OnSceneLoaded;
+        
+        
+    }
+
+    //Kaynaklar  
+    public List<Sprite> playerSprites;
+    public List<Sprite> weaponSprites;
+    public List<int> weaponPrices;
+    public List<int> xpTable;
+
+    //referanslar
+    public player player;
+    public Weapon weapon;
+    public FloatingTextManager floatingTextManager;
+    public RectTransform hitpointBar;
+    public GameObject hud;
+    public GameObject menu;
+    public Animator deathMenuAnim;
+    //logic
+    public int pesos;
+    public int experience;
+
+    public void ShowText(string msg, int fontSize, Color color, Vector3 position, Vector3 motion, float duration)
+    {
+        floatingTextManager.Show(msg, fontSize, color, position, motion, duration);
+    }
+
+    //silah yükseltme
+    public bool TryUpgradeWeapon()
+    {
+        //silah son seviye mi ?
+        if (weaponPrices.Count <= weapon.weaponLevel)
+            return false;
+
+        if(pesos >= weaponPrices[weapon.weaponLevel])
+        {
+            pesos -= weaponPrices[weapon.weaponLevel];
+            weapon.UpgradeWeapon();
+            return true;
+        }
+
+        return false;
+    }
+
+  //hitpoint bar
+  public void OnHitpointChange()
+    {
+        float ratio = (float)player.hitpoint / (float)player.maxHitpoint;
+        hitpointBar.localScale = new Vector3(1, ratio, 1);
+    }
+
+    //exp sys
+    public int GetCurrentLevel()
+    {
+        int r = 0;
+        int add = 0;
+
+        while(experience >= add)
+        {
+            add += xpTable[r];
+            r++;
+
+            if (r == xpTable.Count)//max level mi
+                return r;
+        }
+
+        return r;
+    }
+
+    public int GetXpToLevel(int level)
+    {
+        int r = 0;
+        int xp = 0;
+
+            while(r < level)
+        {
+            xp += xpTable[r];
+            r++;
+        }
+        return xp;
+    }
+    public void GrantXp(int xp)
+    {
+        int currLevel = GetCurrentLevel();
+        experience += xp;
+
+        if (currLevel < GetCurrentLevel())
+            OnLevelUp();
+    }
+
+    public void OnLevelUp()
+    {
+        Debug.Log("Level Up");
+        player.OnLevelUp();
+        OnHitpointChange();
+    }
+
+    //on scene
+    public void OnSceneLoaded(Scene s, LoadSceneMode mode)
+    {
+        player.transform.position = GameObject.Find("SpawnPoint").transform.position;
+    }
+
+    //Death Menu / Resp
+
+    public void Respawn()
+    {
+        
+        deathMenuAnim.SetTrigger("Hide");
+        UnityEngine.SceneManagement.SceneManager.LoadScene("main");
+        player.Respawn();
+
+    }
+    //save alma
+    public void SaveState()
+    {
+        string s = "";
+
+        s += "0" + "|";
+        s += pesos.ToString() + "|";
+        s += experience.ToString() + "|";
+        s += weapon.weaponLevel.ToString();
+
+        PlayerPrefs.SetString("SaveState", s);
+    }
+    //save i yükleme
+    public void LoadState(Scene s, LoadSceneMode mode)
+    {
+        SceneManager.sceneLoaded -= LoadState;
+
+        if (!PlayerPrefs.HasKey("SaveState"))
+            return;
+
+
+        string[] data = PlayerPrefs.GetString("SaveState").Split('|');
+
+        // karakter kostum degistirme
+        pesos = int.Parse(data[1]);
+
+        //exp
+        experience = int.Parse(data[2]);
+        if(GetCurrentLevel() !=1)
+            player.SetLevel(GetCurrentLevel());
+        //silah leveli degistirme
+        weapon.SetWeaponLevel(int.Parse(data[3]));
+
+
+    }
+}
